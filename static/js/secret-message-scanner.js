c617a859fd73d340fd2a4d29036954a444aabb5f
Lyuboslav Petkov <lyuboslav.petkov@gmail.com>
2024-07-18 09:20:08 +0300

    Secret scanner size update

--- static/js/secret-message-scanner.js
+++ static/js/secret-message-scanner.js
@@ -71,7 +71,7 @@ function restartScanner() {
         });
 }
 
-const config = { fps: 10, qrbox: { width: 200, height: 200 } };
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
 html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess)
     .catch(err => {
